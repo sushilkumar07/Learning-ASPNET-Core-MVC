c7ed4d2d13b2b1ce4a44cd0e3862130131c8e9dc
gitName <a@b.c>
2020-08-05 15:42:31 -0400

    Represent the database using Entity Framework

--- ExploreCalifornia/ExploreCalifornia/Models/Post.cs
+++ ExploreCalifornia/ExploreCalifornia/Models/Post.cs
@@ -2,24 +2,45 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ExploreCalifornia.Models
 {
     public class Post
     {
+        public long id { get; set; }
+        private string _Key;
+
+        public string Key
+        {
+            get
+            {
+                if (_Key == null)
+                {
+                    _Key = Regex.Replace(Title.ToLower(), "[^a-z0-9]", "-");
+                }
+                return _Key;
+            }
+            set
+            {
+                _Key = value;
+            }
+        }
+
         [Display(Name = "Post Title")]
         [Required]
         //[DataType(DataType.Password)]
         [DataType(DataType.Text)]
-        [StringLength(100,MinimumLength =5 ,ErrorMessage = "Title must be between 5 to 100 characters" )]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "Title must be between 5 to 100 characters")]
         public string Title { get; set; }
         public DateTime Posted { get; set; }
         public string Author { get; set; }
         [Display(Name = "Post Body")]
         [DataType(DataType.MultilineText)]
         [Required]
-        [StringLength(100,  ErrorMessage = "Body must be between characters long")]
+        [StringLength(100, ErrorMessage = "Body must be between characters long")]
         public string Body { get; set; }
     }
 }
--- ExploreCalifornia/ExploreCalifornia/Startup.cs
+++ ExploreCalifornia/ExploreCalifornia/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ExploreCalifornia.dbcontext;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,6 +32,11 @@ namespace ExploreCalifornia
                 DeveloperException = configuration.GetValue<bool>("EnableDeveloperExceptionPage")
             });
             services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddDbContext<BlogDataContext>(options =>
+            {
+                var connectionString = configuration.GetConnectionString("BlogDataContext");
+                options.UseSqlServer(connectionString);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
