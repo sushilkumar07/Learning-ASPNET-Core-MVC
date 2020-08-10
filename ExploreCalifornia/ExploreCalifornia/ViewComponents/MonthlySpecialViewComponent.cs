762d8959e11ee2569d9a4d782b69773859df7054
gitName <a@b.c>
2020-08-10 12:58:11 -0400

    Enhancing a view component with logic and dynamic markup

--- ExploreCalifornia/ExploreCalifornia/ViewComponents/MonthlySpecialViewComponent.cs
+++ ExploreCalifornia/ExploreCalifornia/ViewComponents/MonthlySpecialViewComponent.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExploreCalifornia.dbcontext;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 
 namespace ExploreCalifornia.ViewComponets
@@ -9,10 +11,21 @@ namespace ExploreCalifornia.ViewComponets
     [ViewComponent]
     public class MonthlySpecialViewComponent : ViewComponent
     {
-        public string Invoke()
+        private readonly BlogDataContext _db;
+        //public string Invoke()
+        //{
+        //    return "TODO : Show monthly specials from View-Component"; 
+        //}
+        public MonthlySpecialViewComponent(BlogDataContext db)
         {
-            return "TODO : Show monthly specials from View - Component"; 
+            this._db = db;
         }
+        public IViewComponentResult Invoke()
+        {
+            var special = _db.MonthlySpecials.ToArray();
+            return View(special);
+        }
+
 
     }
 }
