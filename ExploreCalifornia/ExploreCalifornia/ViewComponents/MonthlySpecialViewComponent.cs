d44c2a5ab90be5fd363b5d3a5b25f90167f0ac15
gitName <a@b.c>
2020-08-10 11:40:56 -0400

    Creating reusable components with view components

---
+++ ExploreCalifornia/ExploreCalifornia/ViewComponents/MonthlySpecialViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreCalifornia.ViewComponets
+{
+    [ViewComponent]
+    public class MonthlySpecialViewComponent : ViewComponent
+    {
+        public string Invoke()
+        {
+            return "TODO : Show monthly specials from View - Component"; 
+        }
+
+    }
+}
